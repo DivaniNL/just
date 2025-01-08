c551c97b226bae88e604d23beb51f24c70eb028d
DivaniNL <dylanvannierop@gmail.com>
2025-01-08 18:32:55 +0100

    #18 finalized dark mode bug

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,19 @@
 const selectMode = document.querySelector(".theme-selector");
 const fetchMode = () => {
     const savedMode = localStorage.getItem("mode");
-    selectMode.value = savedMode;
     if (!savedMode) {
         setTheme("light");
+        console.log("test");
+    }else{
+        selectMode.value = savedMode;
+        setTheme(savedMode);
     }
 };
 
 // Set theme and save to localStorage
 const setTheme = (theme) => {
   localStorage.setItem("mode", theme);
+  document.body.setAttribute("data-theme", theme);
 };
 selectMode.addEventListener("change", (e) => {
     setTheme(e.target.value);
